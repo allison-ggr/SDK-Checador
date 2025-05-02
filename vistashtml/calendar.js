dc17dc69a6134e838ada929d1a91c24b8d055a84
allison-ggr <practicantes.sistemas1@consolnegocios.com>
2025-05-02 08:48:28 -0600

    Vistas

--- vistashtml/calendar.js
+++ vistashtml/calendar.js
@@ -71,36 +71,3 @@ document.addEventListener("DOMContentLoaded", () => {
     // Renderizar el calendario inicial
     renderCalendar(currentDate);
   });
-
-
-
-  /********************************
-  ***** CODIGO DEL SIDEBAR*********
-  *********************************
-  *********************************/
- 
-  document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.querySelector('.menu-icon');
-    const sidebar = document.getElementById('sidebar');
-    const closeBtn = document.getElementById('close-sidebar');
-  
-    menuIcon.addEventListener('click', () => {
-      sidebar.classList.add('open');
-    });
-  
-    closeBtn.addEventListener('click', () => {
-      sidebar.classList.remove('open');
-    });
-  
-    // Cerrar si se hace clic fuera del sidebar
-    document.addEventListener('click', (event) => {
-      if (
-        sidebar.classList.contains('open') &&
-        !sidebar.contains(event.target) &&
-        !menuIcon.contains(event.target)
-      ) {
-        sidebar.classList.remove('open');
-      }
-    });
-  });
-  
