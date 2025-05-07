2362b23bda57ffdc97018c30b1261ed47d866670
jesusavina <practicantes.sistemas2@consolnegocios.com>
2025-05-07 11:43:17 -0600

    formato a usuarios

--- script_mostrar_usuarios.py
+++ script_mostrar_usuarios.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import json
 sys.path.append("zk")
 
 from zk import ZK, const
@@ -7,23 +8,28 @@ from zk import ZK, const
 conn = None
 zk = ZK('192.168.1.200', port=4370, timeout=10, password=0, force_udp=False, ommit_ping=False)
 try:
-
-    print ('Connecting to device ...')
+    print('Connecting to device ...')
     conn = zk.connect()
-    print ('Disabling device ...')
+    print('Disabling device ...')
     conn.disable_device()
-    
+
     users = conn.get_users()
+    user_list = []
+
     for user in users:
-        if user.privilege == const.USER_ADMIN:
-            privilege = 'Admin'
-        else:
-            privilege = 'User'
-        print ('ID: {}, Name: {}, Privilege: {}'.format(user.uid, user.name, privilege))
+        privilege = 'Admin' if user.privilege == const.USER_ADMIN else 'User'
+        user_data = {
+            "user_id": user.user_id,
+            "nombre": user.name,
+            "privilegio": privilege
+        }
+        user_list.append(user_data)
+
+    # Imprimir como JSON formateado
+    print(json.dumps(user_list, indent=4, ensure_ascii=False))
 
 except Exception as e:
-    print ("Process terminate : {}".format(e))
+    print("Process terminated: {}".format(e))
 finally:
     if conn:
         conn.disconnect()
-
